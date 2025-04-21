d59e7a3ffad02e34a455066be7b11ba07dee0981
Matt Mendez Zugenbuehler <mzugenbuehler@gmail.com>
2025-04-20 17:40:55 -0700

    additional edit changes

--- junk_app/views.py
+++ junk_app/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-
 # Create your views here.
 # For every function you create, you need to add it to the urls.py file
 # dummy data 
