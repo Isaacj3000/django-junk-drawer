ee6cfc2dc97c662873830278631c2e40f55a3a38
Isaacj3000 <Isaacjeanbaptiste95@gmail.com>
2025-04-29 00:27:13 -0400

    fixed error

--- junkdrawer/settings.py
+++ junkdrawer/settings.py
@@ -82,15 +82,15 @@ WSGI_APPLICATION = 'junkdrawer.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
-    }
-}
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': os.getenv('DB_NAME'),
+#         'USER': os.getenv('DB_USER'),
+#         'PASSWORD': os.getenv('DB_PASSWORD'),
+#         'HOST': os.getenv('DB_HOST'),
+#         'PORT': os.getenv('DB_PORT'),
+#     }
+# }
 if 'ON_HEROKU' in os.environ:
     DATABASES = {
         "default": dj_database_url.config(
